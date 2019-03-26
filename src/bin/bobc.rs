f1c98fb9b1658b3c6db944f526ac603a6fa712e9
Kirill Bushminkin <kb@qoollo.com>
2019-03-26 15:08:08 +0300

    cliet spawns 100s

--- src/bin/bobc.rs
+++ src/bin/bobc.rs
@@ -1,46 +1,80 @@
 
 use futures::{Future, Poll};
+use futures::stream::futures_unordered;
+use futures::stream::futures_ordered;
+use futures::stream::Stream;
 use tokio::executor::DefaultExecutor;
 use tokio::net::tcp::{ConnectFuture, TcpStream};
 use tower_grpc::Request;
 use tower_h2::client;
 use tower_service::Service;
 use tower::MakeService;
+use std::io;
 
+use bob::api::grpc::client::BobApi;
+use bob::api::grpc::{PutRequest,GetRequest};
+
+use tokio::runtime::{Runtime};
 
 fn main() {
+
+    let mut rt = Runtime::new().unwrap();
     let uri: http::Uri = format!("http://localhost:20000").parse().unwrap();
 
     let h2_settings = Default::default();
-    let mut make_client = client::Connect::new(Dst, h2_settings, DefaultExecutor::current());
+    let mut make_client = client::Connect::new(Dst, h2_settings, rt.executor());
 
-    let pur_req = make_client.make_service(())
-    .map(move |conn| {
-        use bob::api::grpc::client::BobApi;
-
-        let conn = tower_add_origin::Builder::new()
+    let conn_l = rt.block_on(make_client.make_service(())).unwrap();
+    let conn = tower_add_origin::Builder::new()
             .uri(uri)
-            .build(conn)
+            .build(conn_l)
             .unwrap();
+    let mut client = BobApi::new(conn);
 
-        BobApi::new(conn)
-    })
-    .and_then(|mut client| {
-        use bob::api::grpc::PutRequest;
 
-        client.put(Request::new(PutRequest { key: None, data: None, options: None}))
-                .map_err(|e| panic!("gRPC request failed; err={:?}", e))
+    let pur_req:Vec<_> = (0..10).map(|_| client.put(Request::new(
+        PutRequest { key: None, data: None, options: None}))
+    /*            .map_err(|e| println!("gRPC request failed; err={:?}", e))*/
+    .and_then(|response| {
+        println!("RESPONSE = {:?}", response);
+        Ok(())
     })
+    /*.map_err(|e| {
+        println!("ERR = {:?}", e);
+    })*/).collect();
+
+    let rqs = futures_unordered(pur_req).collect();
+    rt.block_on(rqs);
+
+    println!("Press any key to send GET");
+
+    io::stdin().read_line(&mut String::default());
+    let get_req = client.get(Request::new(GetRequest { key: None, options: None}))
+    .map_err(|e| println!("gRPC request failed; err={:?}", e))
     .and_then(|response| {
         println!("RESPONSE = {:?}", response);
         Ok(())
     })
     .map_err(|e| {
         println!("ERR = {:?}", e);
-    });
+    });    
 
-    tokio::run(pur_req);
+    rt.block_on(get_req);
 
+    println!("Press any key to send second GET");
+
+    io::stdin().read_line(&mut String::default());
+    let get_req2 = client.get(Request::new(GetRequest { key: None, options: None}))
+    .map_err(|e| println!("gRPC request failed; err={:?}", e))
+    .and_then(|response| {
+        println!("RESPONSE = {:?}", response);
+        Ok(())
+    })
+    .map_err(|e| {
+        println!("ERR = {:?}", e);
+    });    
+
+    rt.block_on(get_req2);
     // println!("PUT: {:?}", put_resp.wait());
     // let get_req = GetRequest::new();
     // let get_resp = client.get(RequestOptions::new(), get_req);
