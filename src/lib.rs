a8d46140266eb6a685ad588da98a502b41fc491c
Kirill Bushminkin <kb@qoollo.com>
2019-03-29 15:50:23 +0300

    no need for implicit crate import via extern crate

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,5 @@
 
 #[macro_use]
 extern crate bitflags;
-extern crate prost;
-extern crate tokio;
-extern crate http;
-extern crate tower_h2;
-extern crate tower_add_origin;
-extern crate tower_grpc;
-extern crate tower_service;
-extern crate tower;
-extern crate stopwatch;
-extern crate tokio_periodic;
 pub mod api;
 pub mod core;
