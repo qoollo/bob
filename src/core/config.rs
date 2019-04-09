cd6367343ec767a319e5307f3a19691b856fb06f
nmenshov <nikmenshov@mail.ru>
2019-04-09 18:46:34 +0300

    some refactoring

--- src/core/config.rs
+++ src/core/config.rs
@@ -151,72 +151,85 @@ impl Validatable for Cluster {
     }
 }
 
-pub fn read_config(filename: &String) -> Option<Cluster> {
-    let result:Result<String,_> = fs::read_to_string(filename);
-    match result {
-        Ok(config) => return parse_config(&config),
-        Err(e) => {
-            error!("error on file opening: {}", e);
-            return None;
-        }
-    }
-}
-
-pub fn parse_config(config: &String) -> Option<Cluster> {
-    let result:Result<Cluster, _> = serde_yaml::from_str(config);
-    match result {
-        Ok(cluster) => return Some(cluster),
-        Err(e) => {
-            error!("error on yaml parsing: {}", e);
-            return None;
-        }
-    }
-}
-
 use crate::core::data::VDisk as DataVDisk;
 use crate::core::data::NodeDisk as DataNodeDisk;
 use crate::core::data::Node as DataNode;
 
 use std::collections::HashMap;
 
-pub fn conver_to_data(cluster: &Cluster) -> Vec<DataVDisk>  {
-    let mut node_map = HashMap::new();
-    for node in cluster.nodes.iter() {
-        let mut disk_map = HashMap::new();
-        for disk in node.disks.iter() {
-            disk_map.insert(disk.name.clone(), disk.path.clone());
-        }
-        node_map.insert(node.name.clone(), (node.address.split(":").collect::<Vec<&str>>(), disk_map));
-    }
-
-    let mut result: Vec<DataVDisk> = Vec::with_capacity(cluster.vdisks.len());
-    for vdisk in cluster.vdisks.iter() {
-        let mut disk = DataVDisk{
-            id: vdisk.id as u32,
-            replicas: Vec::with_capacity(vdisk.replicas.len())
-        };
-        for replica in vdisk.replicas.iter() {
-            let finded_node = node_map.get(&replica.node).unwrap();
-            let node_disk = DataNodeDisk {
-                path: finded_node.1.get(&replica.disk).unwrap().to_string(),
-                node: DataNode {
-                    host: finded_node.0[0].to_string(),
-                    port: finded_node.0[1].parse().unwrap(),
-                },
-            };
-            disk.replicas.push(node_disk);
-        }
-        result.push(disk);
-    }
-    result
+pub trait BobConfig {
+    fn get_cluster_config(&self, filename: &String) -> Option<Vec<DataVDisk>>;
+    
+    fn read_config(&self, filename: &String) ->Option<Cluster>;
+    fn parse_config(&self, config: &String) -> Option<Cluster>;
+    fn convert_to_data(&self, cluster: &Cluster) -> Vec<DataVDisk>;    
 }
 
-pub fn get_cluster_config(filename: &String) -> Option<Vec<DataVDisk>> {
-    let file: Option<Cluster> = read_config(filename);
-    match file {
-        Some(config) => {
-             return Some(conver_to_data(&config))
-        },
-        _ => return None,
+pub struct YamlConfig { }
+
+impl BobConfig for YamlConfig {
+    fn read_config(&self, filename: &String) -> Option<Cluster> {
+        let result:Result<String,_> = fs::read_to_string(filename);
+        match result {
+            Ok(config) => return self.parse_config(&config),
+            Err(e) => {
+                error!("error on file opening: {}", e);
+                return None;
+            }
+        }
+    }
+    fn parse_config(&self, config: &String) -> Option<Cluster> {
+        let result:Result<Cluster, _> = serde_yaml::from_str(config);
+        match result {
+            Ok(cluster) => return Some(cluster),
+            Err(e) => {
+                error!("error on yaml parsing: {}", e);
+                return None;
+            }
+        }
+    }
+    fn convert_to_data(&self, cluster: &Cluster) -> Vec<DataVDisk>  {
+        let mut node_map = HashMap::new();
+        for node in cluster.nodes.iter() {
+            let mut disk_map = HashMap::new();
+            for disk in node.disks.iter() {
+                disk_map.insert(disk.name.clone(), disk.path.clone());
+            }
+            node_map.insert(node.name.clone(), (node.address.split(":").collect::<Vec<&str>>(), disk_map));
+        }
+
+        let mut result: Vec<DataVDisk> = Vec::with_capacity(cluster.vdisks.len());
+        for vdisk in cluster.vdisks.iter() {
+            let mut disk = DataVDisk{
+                id: vdisk.id as u32,
+                replicas: Vec::with_capacity(vdisk.replicas.len())
+            };
+            for replica in vdisk.replicas.iter() {
+                let finded_node = node_map.get(&replica.node).unwrap();
+                let node_disk = DataNodeDisk {
+                    path: finded_node.1.get(&replica.disk).unwrap().to_string(),
+                    node: DataNode {
+                        host: finded_node.0[0].to_string(),
+                        port: finded_node.0[1].parse().unwrap(),
+                    },
+                };
+                disk.replicas.push(node_disk);
+            }
+            result.push(disk);
+        }
+        result
+    }
+    fn get_cluster_config(&self, filename: &String) -> Option<Vec<DataVDisk>> {
+        let file: Option<Cluster> = self.read_config(filename);
+        match file {
+            Some(config) => {
+                if !config.validate(){
+                    error!("config is not valid");
+                    return None;
+                }
+                return Some(self.convert_to_data(&config))
+            },
+            _ => return None,
+        }
     }
 }
