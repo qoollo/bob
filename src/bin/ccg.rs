777b73c20a6c015ab4d5f2e42151ad1b16368496
Pavel Iakushin <p.yakushin@qoollo.com>
2020-01-15 14:52:10 +0300

    add file

---
+++ src/bin/ccg.rs
@@ -0,0 +1,91 @@
+#[macro_use]
+extern crate log;
+
+use chrono::Local;
+use clap::{App, Arg};
+use env_logger::fmt::Color;
+use log::{Level, LevelFilter};
+use std::io::Write;
+
+#[tokio::main]
+async fn main() {
+    init_logger();
+    let input = read_from_file();
+    let output = generate_config(input);
+    write_to_file(output);
+    debug!("config cluster generation: OK");
+}
+
+fn init_logger() {
+    env_logger::builder()
+        .format(|buf, record: &log::Record| {
+            let mut style = buf.style();
+            let color = match record.level() {
+                Level::Error => Color::Red,
+                Level::Warn => Color::Yellow,
+                Level::Info => Color::Green,
+                Level::Debug => Color::Cyan,
+                Level::Trace => Color::White,
+            };
+            style.set_color(color);
+            writeln!(
+                buf,
+                "[{} {}:{:^4} {:^5}] - {}",
+                Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.module_path().unwrap_or(""),
+                record.line().unwrap_or(0),
+                style.value(record.level()),
+                record.args(),
+            )
+        })
+        .filter_level(LevelFilter::Trace)
+        .try_init()
+        .unwrap_or(());
+    debug!("init logger: OK");
+}
+
+fn read_from_file() -> () {
+    let name = get_name();
+    let file = open_file(name);
+    let content = read_file(file);
+    let res = deserialize(content);
+    debug!("read from file: OK");
+    res
+}
+
+fn get_name() -> String {
+    let input = Arg::with_name("input")
+        .short("i")
+        .default_value("cluster.yaml")
+        .takes_value(true);
+    debug!("input arg: OK");
+    let matches = App::new("Config Cluster Generator")
+        .arg(input)
+        .get_matches();
+    debug!("get matches: OK");
+    let name = matches
+        .value_of("input")
+        .expect("is some, because of default arg value");
+    debug!("get name: {}", name);
+    name.to_owned()
+}
+
+fn open_file(name: String) -> () {
+    debug!("open file: OK");
+}
+
+fn read_file(file: ()) -> () {
+    debug!("read file: OK");
+}
+
+fn deserialize(content: ()) -> () {
+    debug!("deserialize: OK");
+}
+
+fn generate_config(input: ()) -> () {
+    debug!("generate config: OK");
+}
+
+fn write_to_file(output: ()) {
+    debug!("write to file: OK");
+}
