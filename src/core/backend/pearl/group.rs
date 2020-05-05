227da0fc5a7ab2d76059d228e9d33a68106c62c8
pyakushin <jetpilot412@gmail.com>
2020-05-05 17:41:49 +0300

    fix: check vdisk_id even if operation contains remote node name bug led to piling all alien records into one vdisk folder

--- src/core/backend/pearl/group.rs
+++ src/core/backend/pearl/group.rs
@@ -31,25 +31,17 @@ impl Group {
     }
 
     pub fn can_process_operation(&self, operation: &Operation) -> bool {
+        trace!("check {} can process operation {:?}", self, operation);
         if operation.is_data_alien() {
-            match &operation.remote_node_name() {
-                None => self.vdisk_id == operation.vdisk_id(),
-                Some(node_name) => *node_name == self.node_name,
-            }
+            let name_matched = if let Some(node_name) = operation.remote_node_name() {
+                *node_name == self.node_name
+            } else {
+                true
+            };
+            name_matched && self.vdisk_id == operation.vdisk_id()
         } else {
             self.disk_name == operation.disk_name_local() && self.vdisk_id == operation.vdisk_id()
         }
-        // trace!("check {} can process operation {:?}", self, operation);
-        // if operation.is_data_alien() {
-        //     let name_matched = if let Some(node_name) = operation.remote_node_name() {
-        //         *node_name == self.node_name
-        //     } else {
-        //         true
-        //     };
-        //     name_matched && self.vdisk_id == operation.vdisk_id()
-        // } else {
-        //     self.disk_name == operation.disk_name_local() && self.vdisk_id == operation.vdisk_id()
-        // }
     }
 
     pub async fn run(&self) -> Result<(), Error> {
