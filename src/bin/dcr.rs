9bee532a63e995ee3c9a94e4f3c0dbad8f512dd2
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2020-05-30 17:49:01 +0300

    factor out modules from dcr, add suppport for gracefull stop

--- src/bin/dcr.rs
+++ src/bin/dcr.rs
@@ -1,30 +1,40 @@
-use bitflags::_core::cell::RefCell;
-use bob::configs::node::BackendSettings;
-use bob::configs::{Cluster, ClusterNode, MetricsConfig, Node, Pearl, Replica, VDisk};
-use bob::DiskPath;
-use clap::{App, Arg};
-use serde::{Serialize, Serializer};
-use std::collections::HashMap;
-use std::error::Error;
-use std::io::{Read, Stdout, Write};
-use std::net::Ipv4Addr;
-use std::process::{Child, Command, Stdio};
-use std::str::FromStr;
-use std::thread::spawn;
-
 #[macro_use]
 extern crate derive_new;
 #[macro_use]
 extern crate serde_derive;
+extern crate ctrlc;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let configuration = get_configuration()?;
+use crate::docker_cluster_runner::fs_configuration::FSConfiguration;
+use crate::docker_cluster_runner::TestClusterConfiguration;
+use clap::{App, Arg};
+use std::error::Error;
+use std::fs;
+use std::sync::{Arc, Mutex};
+
+mod docker_cluster_runner;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config_dir = "cluster_test";
-    save_configuration(config_dir, &configuration)?;
-    let compose = configuration.create_docker_compose(config_dir, "cluster_test/Dockerfile");
-    let compose_string = serde_yaml::to_string(&compose)?;
-    std::fs::write(format!("{}/docker-compose.yml", config_dir), compose_string)?;
+    let fs_configuration = FSConfiguration::new(
+        ".".to_string(),
+        config_dir.to_string(),
+        config_dir.to_string(),
+        "/tmp".to_string(),
+    );
+    let configuration = get_configuration()?;
+    configuration.save_cluster_configuration(config_dir)?;
+    let compose = configuration.create_docker_compose(fs_configuration, "bobnet".to_string())?;
+    let arc = Arc::new(Mutex::new(Box::new(compose)));
+    let ctrlc_arc = arc.clone();
+    ctrlc::set_handler(move || {
+        let compose = ctrlc_arc.lock().unwrap();
+        compose
+            .down(config_dir)
+            .expect("failed to run \"down\"")
+            .wait_with_output()
+            .expect("failed to wait for compose down otuput");
+    })?;
+    arc.lock().unwrap().up(config_dir)?.wait_with_output()?;
     Ok(())
 }
 
@@ -40,283 +50,7 @@ fn get_configuration() -> Result<TestClusterConfiguration, Box<dyn Error>> {
         )
         .get_matches();
     let config_filename = matches.value_of("config").expect("required");
-    let file_content = std::fs::read_to_string(config_filename)?;
+    let file_content = fs::read_to_string(config_filename)?;
     let configuration: TestClusterConfiguration = serde_yaml::from_str(&file_content)?;
     Ok(configuration)
-}
-
-fn save_configuration(
-    base_dir: &str,
-    configuration: &TestClusterConfiguration,
-) -> Result<(), Box<dyn Error>> {
-    let cluster_configuration = configuration.create_cluster_configuration();
-    let cluster_configuration = serde_yaml::to_string(&cluster_configuration)?;
-    std::fs::write(
-        format!(
-            "{}/{}.yaml",
-            base_dir,
-            TestClusterConfiguration::cluster_filename_without_extension()
-        ),
-        cluster_configuration,
-    )?;
-    for i in 0..configuration.nodes_count {
-        let (name, node) = configuration.create_named_node_configuration(i);
-        let node_configuration = serde_yaml::to_string(&node)?;
-        std::fs::write(format!("{}/{}.yaml", base_dir, name), node_configuration)?;
-    }
-    Ok(())
-}
-
-#[derive(Deserialize, new)]
-struct TestClusterConfiguration {
-    nodes_count: u32,
-    replicas_count: u32,
-    vdisks_count: u32,
-    port: u32,
-}
-
-impl TestClusterConfiguration {
-    fn create_cluster_configuration(&self) -> Cluster {
-        let nodes = self.create_nodes();
-        let vdisks = self.create_vdisks();
-        Cluster::new(nodes, vdisks)
-    }
-
-    fn create_named_node_configuration(&self, node_index: u32) -> (String, Node) {
-        let node = Node::new(
-            "/configs/logger.yaml".to_string(),
-            Self::get_node_name(node_index),
-            1,
-            "3sec".to_string(),
-            "5000ms".to_string(),
-            "simple".to_string(),
-            "pearl".to_string(),
-            Some(self.get_pearl_config()),
-            Some(MetricsConfig::new(
-                "test".to_string(),
-                "127.0.0.1:8888".to_string(),
-            )),
-            RefCell::default(),
-            RefCell::default(),
-        );
-        (Self::get_node_name(node_index), node)
-    }
-
-    fn create_docker_compose(&self, config_dir: &str, dockerfile: &str) -> DockerCompose {
-        let build = DockerBuild::new(
-            std::fs::canonicalize(".")
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_string(),
-            dockerfile.to_string(),
-        );
-        let volumes = vec![
-            VolumeMapping::new("/tmp".to_string(), "/tmp".to_string()),
-            VolumeMapping::new(
-                std::fs::canonicalize(config_dir)
-                    .unwrap()
-                    .to_str()
-                    .unwrap()
-                    .to_string(),
-                "/configs".to_string(),
-            ),
-        ];
-        let mut services = HashMap::with_capacity(self.nodes_count as usize);
-        for node in 0..self.nodes_count {
-            let command = format!(
-                "./bobd -c /configs/{}.yaml -n /configs/{}.yaml",
-                Self::cluster_filename_without_extension(),
-                Self::get_node_name(node)
-            );
-            let network =
-                DockerNetwork::new(Ipv4Addr::from_str(&Self::get_node_addr(node)).unwrap());
-            let mut networks = HashMap::with_capacity(1);
-            networks.insert("bobnet".to_string(), network);
-            services.insert(
-                Self::get_node_name(node),
-                DockerService::new(build.clone(), volumes.clone(), command, networks),
-            );
-        }
-        let mut networks = HashMap::with_capacity(1);
-        networks.insert(
-            "bobnet".to_string(),
-            DockerComposeNetwork::new(
-                "bridge".to_string(),
-                IPAMConfiguration::new(vec![SubnetConfiguration::new(
-                    "192.168.17.0/24".to_string(),
-                )]),
-            ),
-        );
-        DockerCompose::new("3.8".to_string(), services, networks)
-    }
-
-    fn get_pearl_config(&self) -> Pearl {
-        Pearl::new(
-            1000000,
-            10000,
-            "bob".to_string(),
-            "100ms".to_string(),
-            3,
-            Self::get_disk_name(self.vdisks_count - 1),
-            true,
-            BackendSettings::new(
-                "bob".to_string(),
-                "alien".to_string(),
-                "1d".to_string(),
-                "100ms".to_string(),
-            ),
-        )
-    }
-
-    fn create_nodes(&self) -> Vec<ClusterNode> {
-        let mut result = Vec::with_capacity(self.nodes_count as usize);
-        for i in 0..self.nodes_count {
-            let name = Self::get_node_name(i);
-            let address = format!("{}:20000", Self::get_node_addr(i));
-            let disk_paths = self.create_disk_paths(i);
-            result.push(ClusterNode::new(name, address, disk_paths));
-        }
-        result
-    }
-
-    fn create_vdisks(&self) -> Vec<VDisk> {
-        let mut result = Vec::with_capacity(self.vdisks_count as usize);
-        for i in 0..self.vdisks_count {
-            let mut vdisk = VDisk::new(i);
-            for j in 0..self.nodes_count {
-                let node_name = Self::get_node_name(j);
-                let disk_name = Self::get_disk_name(i);
-                vdisk.push_replica(Replica::new(node_name, disk_name));
-            }
-            result.push(vdisk);
-        }
-        result
-    }
-
-    fn create_disk_paths(&self, node_index: u32) -> Vec<DiskPath> {
-        let mut result = Vec::with_capacity(self.vdisks_count as usize);
-        for i in 0..self.vdisks_count {
-            result.push(Self::get_disk_path(node_index, i));
-        }
-        result
-    }
-
-    fn get_node_addr(node_index: u32) -> String {
-        format!("192.168.17.{}", node_index + 10)
-    }
-
-    fn get_disk_path(node_index: u32, disk_index: u32) -> DiskPath {
-        DiskPath::new(
-            Self::get_disk_name(disk_index),
-            format!(
-                "{}/node_{}/disk_{}",
-                Self::base_disk_dir(),
-                node_index,
-                disk_index
-            ),
-        )
-    }
-
-    fn get_disk_name(disk_index: u32) -> String {
-        format!("disk_{}", disk_index)
-    }
-
-    fn get_node_name(i: u32) -> String {
-        format!("node_{}", i)
-    }
-
-    const fn base_disk_dir() -> &'static str {
-        "/tmp"
-    }
-
-    const fn cluster_filename_without_extension() -> &'static str {
-        "cluster"
-    }
-}
-
-#[derive(new)]
-struct TestCluster {
-    cluster: Cluster,
-    docker_compose: DockerCompose,
-}
-
-#[derive(Serialize, new)]
-struct DockerCompose {
-    version: String,
-    services: HashMap<String, DockerService>,
-    networks: HashMap<String, DockerComposeNetwork>,
-}
-
-#[derive(Serialize, new)]
-struct DockerService {
-    build: DockerBuild,
-    volumes: Vec<VolumeMapping>,
-    command: String,
-    networks: HashMap<String, DockerNetwork>,
-}
-
-#[derive(Serialize, new, Clone)]
-struct DockerBuild {
-    context: String,
-    dockerfile: String,
-}
-
-impl DockerBuild {
-    fn build(&self) -> Result<Child, Box<dyn Error>> {
-        let mut command = Command::new("sudo");
-        command
-            .arg("docker")
-            .arg("build")
-            .arg(self.context.clone())
-            .arg("-f")
-            .arg(self.dockerfile.clone());
-        command.spawn().map_err(|e| e.into())
-    }
-}
-
-#[derive(new, Clone)]
-struct VolumeMapping {
-    host_dir: String,
-    docker_dir: String,
-}
-
-impl Serialize for VolumeMapping {
-    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_str(&format!("{}:{}", self.host_dir, self.docker_dir))
-    }
-}
-
-#[derive(Serialize, Clone, Copy, new)]
-struct DockerNetwork {
-    ipv4_address: Ipv4Addr,
-}
-
-#[derive(Serialize, new)]
-struct DockerComposeNetwork {
-    driver: String,
-    ipam: IPAMConfiguration,
-}
-
-#[derive(Serialize, new)]
-struct IPAMConfiguration {
-    config: Vec<SubnetConfiguration>,
-}
-
-#[derive(Serialize, new)]
-struct SubnetConfiguration {
-    subnet: String,
-}
-
-mod tests {
-    #[test]
-    fn creates_cluster_configuration_for_two_nodes() {
-        let configuration = TestClusterConfiguration::new(2, 1, 2, 80);
-        let cluster = configuration.create_cluster_configuration();
-        assert_eq!(cluster.nodes().len(), 2, "wrong nodes count");
-        assert_eq!(cluster.vdisks().len(), 2, "wrong vdisks count");
-    }
 }
---
+++ src/bin/docker_cluster_runner/mod.rs
@@ -0,0 +1,236 @@
+use crate::docker_cluster_runner::docker_compose_wrapper::*;
+use crate::docker_cluster_runner::fs_configuration::FSConfiguration;
+use bitflags::_core::cell::RefCell;
+use bob::configs::node::BackendSettings;
+use bob::configs::{Cluster, ClusterNode, MetricsConfig, Node, Pearl, Replica, VDisk};
+use bob::DiskPath;
+use filesystem_constants::DockerFSConstants;
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs;
+use std::net::Ipv4Addr;
+use std::str::FromStr;
+
+#[derive(Deserialize, new)]
+pub struct TestClusterConfiguration {
+    nodes_count: u32,
+    vdisks_count: u32,
+}
+
+impl TestClusterConfiguration {
+    pub fn save_cluster_configuration(&self, directory: &str) -> Result<(), Box<dyn Error>> {
+        let cluster = self.create_cluster();
+        let cluster_string = serde_yaml::to_string(&cluster)?;
+        fs::write(format!("{}/cluster.yaml", directory), cluster_string)?;
+        for node in 0..self.nodes_count {
+            let (name, node) = self.create_named_node_configuration(node);
+            let node_string = serde_yaml::to_string(&node)?;
+            fs::write(format!("{}/{}.yaml", directory, name), node_string)?;
+        }
+        Ok(())
+    }
+
+    pub fn create_docker_compose(
+        &self,
+        fs_configuration: FSConfiguration,
+        network_name: String,
+    ) -> Result<DockerCompose, Box<dyn Error>> {
+        let bob_dir = Self::convert_to_absolute_path(&fs_configuration.bob_source_dir)?;
+        let config_dir =
+            Self::convert_to_absolute_path(&fs_configuration.cluster_configuration_dir)?;
+        let disks_dir = Self::convert_to_absolute_path(&fs_configuration.disks_dir)?;
+        let build = DockerBuild::new(
+            bob_dir,
+            format!("{}/Dockerfile", fs_configuration.dockerfile_path),
+        );
+        let volumes = vec![
+            VolumeMapping::new(disks_dir, DockerFSConstants::docker_disks_dir()),
+            VolumeMapping::new(config_dir.clone(), DockerFSConstants::docker_configs_dir()),
+        ];
+        let mut services = HashMap::with_capacity(self.nodes_count as usize);
+        for node in 0..self.nodes_count {
+            let command = Self::create_docker_command(node);
+            let networks = Self::create_networks_with_single_network(node, network_name.clone());
+            services.insert(
+                Self::get_node_name(node),
+                DockerService::new(build.clone(), volumes.clone(), command, networks),
+            );
+        }
+        let networks = Self::create_networks_with_custom_network(network_name);
+        let compose = DockerCompose::new("3.8".to_string(), services, networks);
+        Ok(compose)
+    }
+
+    fn create_networks_with_custom_network(
+        network_name: String,
+    ) -> HashMap<String, DockerComposeNetwork> {
+        let mut networks = HashMap::with_capacity(1);
+        networks.insert(
+            network_name,
+            DockerComposeNetwork::new(
+                "bridge".to_string(),
+                IPAMConfiguration::new(vec![SubnetConfiguration::new(format!(
+                    "{}0/24",
+                    Self::ip_addr_prefix()
+                ))]),
+            ),
+        );
+        networks
+    }
+
+    fn convert_to_absolute_path(dir: &str) -> Result<String, Box<dyn Error>> {
+        fs::canonicalize(dir)
+            .map(|p| p.to_str().unwrap().to_string())
+            .map_err(|e| e.into())
+    }
+
+    fn create_networks_with_single_network(
+        node: u32,
+        network_name: String,
+    ) -> HashMap<String, DockerNetwork> {
+        let network = DockerNetwork::new(Ipv4Addr::from_str(&Self::get_node_addr(node)).unwrap());
+        let mut networks = HashMap::with_capacity(1);
+        networks.insert(network_name, network);
+        networks
+    }
+
+    fn create_docker_command(node: u32) -> String {
+        let command = format!(
+            "./bobd -c {}/cluster.yaml -n {}/{}.yaml",
+            DockerFSConstants::docker_configs_dir(),
+            DockerFSConstants::docker_configs_dir(),
+            Self::get_node_name(node)
+        );
+        command
+    }
+
+    fn create_cluster(&self) -> Cluster {
+        let nodes = self.create_nodes();
+        let vdisks = self.create_vdisks();
+        Cluster::new(nodes, vdisks)
+    }
+
+    fn create_named_node_configuration(&self, node_index: u32) -> (String, Node) {
+        let node = Node::new(
+            format!("{}/logger.yaml", DockerFSConstants::docker_configs_dir()),
+            Self::get_node_name(node_index),
+            1,
+            "3sec".to_string(),
+            "5000ms".to_string(),
+            "simple".to_string(),
+            "pearl".to_string(),
+            Some(self.get_pearl_config()),
+            Some(MetricsConfig::new(
+                "stub".to_string(),
+                "127.0.0.1:8888".to_string(),
+            )),
+            RefCell::default(),
+            RefCell::default(),
+        );
+        (Self::get_node_name(node_index), node)
+    }
+
+    fn get_pearl_config(&self) -> Pearl {
+        Pearl::new(
+            1000000,
+            10000,
+            "bob".to_string(),
+            "100ms".to_string(),
+            3,
+            Self::get_disk_name(self.vdisks_count - 1),
+            true,
+            BackendSettings::new(
+                "bob".to_string(),
+                "alien".to_string(),
+                "1d".to_string(),
+                "100ms".to_string(),
+            ),
+        )
+    }
+
+    fn create_nodes(&self) -> Vec<ClusterNode> {
+        let mut result = Vec::with_capacity(self.nodes_count as usize);
+        for i in 0..self.nodes_count {
+            let name = Self::get_node_name(i);
+            let address = format!("{}:20000", Self::get_node_addr(i));
+            let disk_paths = self.create_disk_paths(i);
+            result.push(ClusterNode::new(name, address, disk_paths));
+        }
+        result
+    }
+
+    fn create_vdisks(&self) -> Vec<VDisk> {
+        let mut result = Vec::with_capacity(self.vdisks_count as usize);
+        for i in 0..self.vdisks_count {
+            let mut vdisk = VDisk::new(i);
+            for j in 0..self.nodes_count {
+                let node_name = Self::get_node_name(j);
+                let disk_name = Self::get_disk_name(i);
+                vdisk.push_replica(Replica::new(node_name, disk_name));
+            }
+            result.push(vdisk);
+        }
+        result
+    }
+
+    fn create_disk_paths(&self, node_index: u32) -> Vec<DiskPath> {
+        let mut result = Vec::with_capacity(self.vdisks_count as usize);
+        for i in 0..self.vdisks_count {
+            result.push(Self::get_disk_path(node_index, i));
+        }
+        result
+    }
+
+    fn get_node_addr(node_index: u32) -> String {
+        format!("{}{}", Self::ip_addr_prefix(), node_index + 10)
+    }
+
+    fn get_disk_path(node_index: u32, disk_index: u32) -> DiskPath {
+        DiskPath::new(
+            Self::get_disk_name(disk_index),
+            format!(
+                "{}/node_{}/disk_{}",
+                DockerFSConstants::docker_disks_dir(),
+                node_index,
+                disk_index
+            ),
+        )
+    }
+
+    fn get_disk_name(disk_index: u32) -> String {
+        format!("disk_{}", disk_index)
+    }
+
+    fn get_node_name(i: u32) -> String {
+        format!("node_{}", i)
+    }
+
+    const fn ip_addr_prefix() -> &'static str {
+        "192.168.17."
+    }
+}
+
+pub mod fs_configuration {
+
+    #[derive(new)]
+    pub struct FSConfiguration {
+        pub(super) bob_source_dir: String,
+        pub(super) cluster_configuration_dir: String,
+        pub(super) dockerfile_path: String,
+        pub(super) disks_dir: String,
+    }
+}
+
+mod filesystem_constants;
+
+pub mod docker_compose_wrapper;
+
+mod tests {
+    #[test]
+    fn creates_cluster_configuration_for_two_nodes() {
+        let configuration = TestClusterConfiguration::new(2, 1, 2, 80);
+        let cluster = configuration.create_cluster_configuration();
+        assert_eq!(cluster.nodes().len(), 2, "wrong nodes count");
+        assert_eq!(cluster.vdisks().len(), 2, "wrong vdisks count");
+    }
+}
