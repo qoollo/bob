5d7af26111b70404c29dbc6736f85edbc544c92d
pyakushin <jetpilot412@gmail.com>
2020-06-26 12:52:52 +0300

    remove get_count check and cluster test with one node because now we look directly to local backend without calling through bob_client, therefore mock fns don't work

--- src/core/cluster/quorum.rs
+++ src/core/cluster/quorum.rs
@@ -336,6 +336,13 @@ mod tests {
     };
     use std::sync::atomic::{AtomicU64, Ordering};
 
+    fn init_logger() {
+        env_logger::builder()
+            .filter_level(log::LevelFilter::Debug)
+            .try_init()
+            .unwrap();
+    }
+
     fn ping_ok(client: &mut BobClient, node: Node) {
         let cl = node;
 
@@ -359,6 +366,7 @@ mod tests {
     }
 
     fn get_ok_timestamp(client: &mut BobClient, node: Node, call: Arc<CountCall>, timestamp: u64) {
+        info!("get ok timestamp");
         client.expect_get().returning(move |_key, _options| {
             call.get_inc();
             test_utils::get_ok(node.name().to_owned(), timestamp)
@@ -366,7 +374,9 @@ mod tests {
     }
 
     fn get_err(client: &mut BobClient, node: Node, call: Arc<CountCall>) {
+        info!("get err");
         client.expect_get().returning(move |_key, _options| {
+            info!("mock client returning closure");
             call.get_inc();
             test_utils::get_err(node.name().to_owned())
         });
@@ -394,11 +404,8 @@ mod tests {
         }
 
         fn get_inc(&self) {
+            debug!("increment get count");
             self.get_count.fetch_add(1, Ordering::SeqCst);
-        }
-
-        fn get_count(&self) -> u64 {
-            self.get_count.load(Ordering::Relaxed)
         }
     }
 
@@ -438,40 +445,34 @@ mod tests {
     }
 
     fn create_ok_node(name: &str, op: (bool, bool)) -> (&str, Call, Arc<CountCall>) {
+        info!("create ok node: {}, op: {:?}", name, op);
         create_node(name, (op.0, op.1, 0))
     }
 
     fn create_node(name: &str, op: (bool, bool, u64)) -> (&str, Call, Arc<CountCall>) {
-        (
-            name,
-            Box::new(
-                move |client: &mut BobClient, n: Node, call: Arc<CountCall>| {
-                    let f = |client: &mut BobClient,
-                             n: Node,
-                             c: Arc<CountCall>,
-                             op: (bool, bool, u64)| {
-                        ping_ok(client, n.clone());
-                        if op.0 {
-                            put_ok(client, n.clone(), c.clone());
-                        } else {
-                            put_err(client, n.clone(), c.clone());
-                        }
-                        if op.1 {
-                            get_ok_timestamp(client, n, c, op.2);
-                        } else {
-                            get_err(client, n, c);
-                        }
-                    };
-                    f(client, n.clone(), call.clone(), op);
-                    client.expect_clone().returning(move || {
-                        let mut cl = BobClient::default();
-                        f(&mut cl, n.clone(), call.clone(), op);
-                        cl
-                    });
-                },
-            ),
-            Arc::new(CountCall::new()),
-        )
+        let call = move |client: &mut BobClient, n: Node, call: Arc<CountCall>| {
+            let f = |client: &mut BobClient, n: Node, c: Arc<CountCall>, op: (bool, bool, u64)| {
+                ping_ok(client, n.clone());
+                if op.0 {
+                    put_ok(client, n.clone(), c.clone());
+                } else {
+                    put_err(client, n.clone(), c.clone());
+                }
+                if op.1 {
+                    get_ok_timestamp(client, n, c, op.2);
+                } else {
+                    get_err(client, n, c);
+                }
+            };
+            f(client, n.clone(), call.clone(), op);
+            client.expect_clone().returning(move || {
+                let mut cl = BobClient::default();
+                f(&mut cl, n.clone(), call.clone(), op);
+                cl
+            });
+        };
+        let call = Box::new(call);
+        (name, call, Arc::new(CountCall::new()))
     }
 
     type Call = Box<dyn Fn(&mut BobClient, Node, Arc<CountCall>)>;
@@ -752,50 +753,21 @@ mod tests {
         assert!(get.is_ok());
     }
 
-    //////////////////////////////////////////////////
-    ////////////////////////////////////////////////// get
-    //////////////////////////////////////////////////
-
-    /// 1 node, 1 vdisk, 1 replics in vdisk, quorum = 1
-    /// get data => ok
-    #[tokio::test]
-    async fn simple_one_node_get_ok() {
-        // log4rs::init_file("./logger.yaml", Default::default()).unwrap();
-        let (vdisks, node, cluster) = prepare_configs(1, 1, 1, 1);
-
-        let actions: Vec<(&str, Call, Arc<CountCall>)> = vec![create_ok_node("0", (true, true))];
-
-        let calls: Vec<_> = actions
-            .iter()
-            .map(|(name, _, call)| ((*name).to_string(), call.clone()))
-            .collect();
-        let (quorum, _) = create_cluster(vdisks, &node, &cluster, &actions).await;
-
-        let result = quorum.get(101).await;
-
-        assert!(result.is_ok());
-        assert_eq!(1, calls[0].1.get_count());
-    }
-
     /// 1 node, 1 vdisk, 1 replics in vdisk, quorum = 1
     /// get no data => err
     #[tokio::test]
     async fn simple_one_node_get_err() {
-        // log4rs::init_file("./logger.yaml", Default::default()).unwrap();
+        // init_logger();
+        info!("logger initialized");
         let (vdisks, node, cluster) = prepare_configs(1, 1, 1, 1);
-
+        info!("configs prepared");
         let actions: Vec<(&str, Call, Arc<CountCall>)> = vec![create_ok_node("0", (true, false))];
-
-        let calls: Vec<_> = actions
-            .iter()
-            .map(|(name, _, call)| ((*name).to_string(), call.clone()))
-            .collect();
+        info!("actions created");
         let (quorum, _) = create_cluster(vdisks, &node, &cluster, &actions).await;
-
+        info!("cluster created");
         let result = quorum.get(102).await;
-
+        info!("request finished");
         assert!(result.is_err());
-        assert_eq!(1, calls[0].1.get_count());
     }
 
     /// 2 nodes, 1 vdisk, 2 replics in vdisk, quorum = 2
@@ -810,17 +782,12 @@ mod tests {
             create_node("1", (true, true, 1)),
         ];
 
-        let calls: Vec<_> = actions
-            .iter()
-            .map(|(name, _, call)| ((*name).to_string(), call.clone()))
-            .collect();
         let (quorum, _) = create_cluster(vdisks, &node, &cluster, &actions).await;
 
         let result = quorum.get(110).await;
 
         assert!(result.is_ok());
         assert_eq!(1, result.unwrap().meta().timestamp());
-        assert_eq!(1, calls[0].1.get_count());
     }
 
     // 2 nodes, 2 vdisk, 1 replics in vdisk, quorum = 1
