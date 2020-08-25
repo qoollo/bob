4eed383f528b6747da297d95dff1a2322dcbc822
pyakushin <jetpilot412@gmail.com>
2020-08-25 17:00:22 +0300

    fix pearl only minor versions. update logs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![type_length_limit = "3167199"]
+#![type_length_limit = "3233604"]
 #![feature(proc_macro_hygiene, decl_macro, drain_filter)]
 #![warn(clippy::pedantic)]
 #![allow(clippy::used_underscore_binding)]
