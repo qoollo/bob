fa431880d48807d6205e7e047747d879c7f66b2c
pyakushin <jetpilot412@gmail.com>
2020-12-16 20:24:20 +0300

    impl From<PathBuf> for WorkDir

--- src/core/backend/pearl/work_dir.rs
+++ src/core/backend/pearl/work_dir.rs
@@ -6,7 +6,7 @@ pub struct WorkDir {
     path: PathBuf,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum MountPoint {
     Dir,
     Device,
@@ -28,6 +28,28 @@ impl WorkDir {
 
 impl From<PathBuf> for WorkDir {
     fn from(path: PathBuf) -> Self {
-        Self { path }
+        let mount_point = if path.to_str().unwrap().starts_with("/dev/sd") {
+            MountPoint::Device
+        } else {
+            MountPoint::Dir
+        };
+        Self { path, mount_point }
     }
+}
+
+#[cfg(test)]
+#[tokio::test]
+async fn test_work_dir_from_path_buf() {
+    let path1 = PathBuf::from("/dev/sdd");
+    let path2 = PathBuf::from("/dev/sdd/jia/ojh/vuj");
+    let path3 = PathBuf::from("/dev/s");
+    let path4 = PathBuf::from("dev/sdd");
+    let path5 = PathBuf::from("/de/sda");
+    let path6 = PathBuf::from("/home/rust");
+    assert!(WorkDir::from(path1).mount_point == MountPoint::Device);
+    assert!(WorkDir::from(path2).mount_point == MountPoint::Device);
+    assert!(WorkDir::from(path3).mount_point == MountPoint::Dir);
+    assert!(WorkDir::from(path4).mount_point == MountPoint::Dir);
+    assert!(WorkDir::from(path5).mount_point == MountPoint::Dir);
+    assert!(WorkDir::from(path6).mount_point == MountPoint::Dir);
 }
