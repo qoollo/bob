44c217c97cd5db98f997553c63f154b54dfe0f2d
pyakushin <jetpilot412@gmail.com>
2021-02-19 16:36:49 +0300

    move generated grpc mod to subcrate

---
+++ bob-common/src/data.rs
@@ -0,0 +1,257 @@
+use bob_grpc::PutOptions;
+use std::hash::Hash;
+
+pub type BobKey = u64;
+
+pub type VDiskID = u32;
+
+impl PutOptions {
+    pub(crate) fn new_local() -> Self {
+        PutOptions {
+            remote_nodes: vec![],
+            force_node: true,
+            overwrite: false,
+        }
+    }
+
+    pub(crate) fn new_alien(remote_nodes: Vec<String>) -> Self {
+        PutOptions {
+            remote_nodes,
+            force_node: true,
+            overwrite: false,
+        }
+    }
+}
+
+impl GetOptions {
+    pub(crate) fn new_local() -> Self {
+        GetOptions {
+            force_node: true,
+            source: GetSource::Normal as i32,
+        }
+    }
+
+    pub(crate) fn new_alien() -> Self {
+        GetOptions {
+            force_node: true,
+            source: GetSource::Alien as i32,
+        }
+    }
+
+    pub(crate) fn new_all() -> Self {
+        GetOptions {
+            force_node: true,
+            source: GetSource::All as i32,
+        }
+    }
+}
+
+impl From<i32> for GetSource {
+    fn from(value: i32) -> Self {
+        match value {
+            0 => GetSource::All,
+            1 => GetSource::Normal,
+            2 => GetSource::Alien,
+            other => {
+                error!("cannot convert value: {} to 'GetSource' enum", other);
+                panic!("fatal core error");
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct BobData {
+    inner: Vec<u8>,
+    meta: BobMeta,
+}
+
+impl BobData {
+    pub(crate) fn new(inner: Vec<u8>, meta: BobMeta) -> Self {
+        BobData { inner, meta }
+    }
+
+    pub(crate) fn inner(&self) -> &[u8] {
+        &self.inner
+    }
+
+    pub(crate) fn into_inner(self) -> Vec<u8> {
+        self.inner
+    }
+
+    pub(crate) fn meta(&self) -> &BobMeta {
+        &self.meta
+    }
+}
+
+impl Debug for BobData {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        f.debug_struct("BobData")
+            .field("len", &self.inner.len())
+            .field("meta", self.meta())
+            .finish()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct BobMeta {
+    timestamp: u64,
+}
+impl BobMeta {
+    pub(crate) fn new(timestamp: u64) -> Self {
+        Self { timestamp }
+    }
+
+    #[inline]
+    pub(crate) fn timestamp(&self) -> u64 {
+        self.timestamp
+    }
+
+    pub(crate) fn stub() -> Self {
+        BobMeta { timestamp: 1 }
+    }
+}
+
+bitflags! {
+    #[derive(Default)]
+    pub(crate) struct BobFlags: u8 {
+        const FORCE_NODE = 0x01;
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct BobOptions {
+    flags: BobFlags,
+    remote_nodes: Vec<String>,
+    get_source: Option<GetSource>,
+}
+
+impl BobOptions {
+    pub(crate) fn new_put(options: Option<PutOptions>) -> Self {
+        let mut flags = BobFlags::default();
+        let remote_nodes = options.map_or(Vec::new(), |vopts| {
+            if vopts.force_node {
+                flags |= BobFlags::FORCE_NODE;
+            }
+            vopts.remote_nodes
+        });
+        BobOptions {
+            flags,
+            remote_nodes,
+            get_source: None,
+        }
+    }
+
+    pub(crate) fn new_get(options: Option<GetOptions>) -> Self {
+        let mut flags = BobFlags::default();
+
+        let get_source = options.map(|vopts| {
+            if vopts.force_node {
+                flags |= BobFlags::FORCE_NODE;
+            }
+            GetSource::from(vopts.source)
+        });
+        BobOptions {
+            flags,
+            remote_nodes: Vec::new(),
+            get_source,
+        }
+    }
+
+    pub(crate) fn remote_nodes(&self) -> &[String] {
+        &self.remote_nodes
+    }
+
+    pub(crate) fn flags(&self) -> BobFlags {
+        self.flags
+    }
+
+    pub(crate) fn get_normal(&self) -> bool {
+        self.get_source.map_or(false, |value| {
+            value == GetSource::All || value == GetSource::Normal
+        })
+    }
+
+    pub(crate) fn get_alien(&self) -> bool {
+        self.get_source.map_or(false, |value| {
+            value == GetSource::All || value == GetSource::Alien
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct VDisk {
+    id: VDiskID,
+    replicas: Vec<NodeDisk>,
+    nodes: Vec<Node>,
+}
+
+impl VDisk {
+    pub(crate) fn new(id: VDiskID) -> Self {
+        VDisk {
+            id,
+            replicas: Vec::new(),
+            nodes: Vec::new(),
+        }
+    }
+
+    pub(crate) fn id(&self) -> VDiskID {
+        self.id
+    }
+
+    pub(crate) fn replicas(&self) -> &[NodeDisk] {
+        &self.replicas
+    }
+
+    pub(crate) fn nodes(&self) -> &[Node] {
+        &self.nodes
+    }
+
+    pub(crate) fn push_replica(&mut self, value: NodeDisk) {
+        self.replicas.push(value)
+    }
+
+    pub(crate) fn set_nodes(&mut self, nodes: &NodesMap) {
+        nodes.values().for_each(|node| {
+            if self.replicas.iter().any(|r| r.node_name() == node.name()) {
+                //TODO check if some duplicates
+                self.nodes.push(node.clone());
+            }
+        })
+    }
+}
+
+/// Structure represents disk on the node. Contains path to disk and name.
+#[derive(Debug, PartialEq, Eq, Clone, Hash, Serialize, Deserialize)]
+pub struct DiskPath {
+    name: String,
+    path: String,
+}
+
+impl DiskPath {
+    /// Creates new `DiskPath` with disk's name and path.
+    #[must_use = "memory allocation"]
+    pub fn new(name: String, path: String) -> DiskPath {
+        DiskPath { name, path }
+    }
+
+    /// Returns disk name.
+    #[must_use]
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub(crate) fn path(&self) -> &str {
+        &self.path
+    }
+}
+
+// @TODO maybe merge NodeDisk and DiskPath
+impl From<&NodeDisk> for DiskPath {
+    fn from(node: &NodeDisk) -> Self {
+        DiskPath {
+            name: node.disk_name().to_owned(),
+            path: node.disk_path().to_owned(),
+        }
+    }
+}
---
+++ bob-grpc/build.rs
@@ -0,0 +1,16 @@
+use std::path::PathBuf;
+
+extern crate tonic_build;
+
+fn main() {
+    let path: PathBuf = format!("{}/src", env!("CARGO_MANIFEST_DIR")).into();
+    if !path.join("bob_storage.rs").exists() {
+        tonic_build::configure()
+            .build_server(true)
+            .build_client(true)
+            .format(false)
+            .out_dir(format!("{}/src", env!("CARGO_MANIFEST_DIR")))
+            .compile(&["proto/bob.proto"], &["proto"])
+            .expect("protobuf compilation");
+    }
+}
