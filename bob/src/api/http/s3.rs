0ae0f5f16154769b00220afa45ad838b31f47e5b
Vladimir Stepanenko <vovac12@gmail.com>
2021-03-24 19:42:19 +0300

    Use default bucket name

--- bob/src/api/http/s3.rs
+++ bob/src/api/http/s3.rs
@@ -1,29 +1,31 @@
+use super::StatusExt;
 use crate::server::Server as BobServer;
 use bob_common::data::{BobKey, BobOptions};
-use rocket::{get, http::ContentType, http::Status, put, response::Content, Data, Route, State};
-
-use super::StatusExt;
+use rocket::{
+    get,
+    http::{ContentType, Status},
+    put,
+    response::Content,
+    Data, Route, State,
+};
 
 pub fn routes() -> impl Into<Vec<Route>> {
-    routes![get_object]
+    routes![get_object, put_object]
 }
 
-#[get("/<_bucket>/<key>")]
+#[get("/default/<key>")]
 pub(crate) fn get_object(
     bob: State<BobServer>,
-    _bucket: String,
     key: BobKey,
 ) -> Result<Content<Vec<u8>>, StatusExt> {
     let opts = BobOptions::new_get(None);
     let result = super::runtime()
         .block_on(async { bob.grinder().get(key, &opts).await })
-        .map_err(|_err| -> StatusExt {
-            StatusExt::new(Status::InternalServerError, false, "Error".into())
-        })?;
+        .map_err(|err| -> StatusExt { err.into() })?;
     Ok(Content(ContentType::Any, result.inner().to_owned()))
 }
 
-#[put("/<_bucket>/<_key>", data = "<_data>")]
-pub fn put_object(_bob: State<BobServer>, _bucket: String, _key: i64, _data: Data) -> String {
-    "String".into()
+#[put("/default/<key>", data = "<data>")]
+pub fn put_object(bob: State<BobServer>, key: BobKey, data: Data) -> Status {
+    Status::NotImplemented
 }
