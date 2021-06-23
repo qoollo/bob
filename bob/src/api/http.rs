9a6ec440c4f5931c7c3c2f2aeec0c56b2360611a
Vladimir Stepanenko <vovac12@gmail.com>
2021-06-23 12:27:07 +0300

    Big integer parsing

--- bob/src/api/http.rs
+++ bob/src/api/http.rs
@@ -656,13 +656,9 @@ impl DataKey {
         Ok(Self(BobKey::from(bytes)))
     }
 
-    fn from_u128(value: u128) -> Result<Self, StatusExt> {
-        Self::from_bytes(value.to_be_bytes().into())
-    }
-
     fn from_guid(guid: &str) -> Result<Self, StatusExt> {
         let guid = uuid::Uuid::from_str(guid).map_err(|e| parse_error(e.to_string()))?;
-        Self::from_u128(guid.as_u128())
+        Self::from_bytes(guid.as_u128().to_be_bytes().into())
     }
 
     fn from_hex(hex: &str) -> Result<Self, StatusExt> {
@@ -684,11 +680,32 @@ impl DataKey {
     }
 
     fn from_decimal(decimal: &str) -> Result<Self, StatusExt> {
-        let key = decimal
-            .parse::<u128>()
-            .map_err(|e| parse_error(e.to_string()))?;
-        Self::from_u128(key)
+        let mut bignum = vec![0];
+        for c in decimal.as_bytes() {
+            let c = *c - '0' as u8;
+            bignum = muladd_bignum(bignum, 10, c as u64);
+        }
+        let bignum = bignum
+            .into_iter()
+            .rev()
+            .flat_map(|v| v.to_be_bytes())
+            .collect();
+        Self::from_bytes(bignum)
     }
+}
+
+fn muladd_bignum(mut bignum: Vec<u64>, mul: u64, add: u64) -> Vec<u64> {
+    let mul = mul as u128;
+    let mut add = add as u128;
+    for val in bignum.iter_mut() {
+        let new_val = *val as u128 * mul + add;
+        *val = (new_val % u64::MAX as u128) as u64;
+        add = new_val / u64::MAX as u128;
+    }
+    if add != 0 {
+        bignum.push(add as u64);
+    }
+    bignum
 }
 
 impl<'r> FromParam<'r> for DataKey {
