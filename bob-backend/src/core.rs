f7956157d2219923ab76142ce237ab35b5569ded
pyakushin <jetpilot412@gmail.com>
2021-06-23 16:55:52 +0300

    add DiskEventsLogger error (#230)

--- bob-backend/src/core.rs
+++ bob-backend/src/core.rs
@@ -80,7 +80,7 @@ impl Operation {
 }
 
 #[async_trait]
-pub trait BackendStorage: Debug  + MetricsProducer + Send + Sync + 'static {
+pub trait BackendStorage: Debug + MetricsProducer + Send + Sync + 'static {
     async fn run_backend(&self) -> AnyResult<()>;
     async fn run(&self) -> AnyResult<()> {
         gauge!(BACKEND_STATE, BACKEND_STARTING);
@@ -136,7 +136,12 @@ impl Backend {
         let inner: Arc<dyn BackendStorage> = match config.backend_type() {
             BackendType::InMemory => Arc::new(MemBackend::new(&mapper)),
             BackendType::Stub => Arc::new(StubBackend {}),
-            BackendType::Pearl => Arc::new(Pearl::new(mapper.clone(), config).await),
+            BackendType::Pearl => {
+                let pearl = Pearl::new(mapper.clone(), config)
+                    .await
+                    .expect("pearl initialization failed");
+                Arc::new(pearl)
+            }
         };
         Self { inner, mapper }
     }
