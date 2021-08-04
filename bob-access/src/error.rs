cdfc98e8aafbea8c81688a25cd4ac027e683a84e
Vladimir Stepanenko <vovac12@gmail.com>
2021-08-04 12:13:57 +0300

    #211 Credentials extraction (#222)
    * Credentials extraction
    
    * Always extract address
    
    * Add CredentialsBuilder
    
    * Remove Unknown error type
    
    * Replace if with match
    
    * Use pattern matching in all MultiExtractor impl's

--- bob-access/src/error.rs
+++ bob-access/src/error.rs
@@ -1,9 +1,33 @@
+use tonic::metadata::errors::ToStrError;
+
 #[derive(Debug)]
 pub struct Error {
     kind: Kind,
 }
 
+impl Error {
+    pub fn credentials_not_provided(message: impl Into<String>) -> Self {
+        Self {
+            kind: Kind::CredentialsNotProvided(message.into()),
+        }
+    }
+
+    pub fn conversion_error(error: ToStrError) -> Self {
+        Self {
+            kind: Kind::ConversionError(error),
+        }
+    }
+
+    pub fn multiple_credentials_types() -> Self {
+        Self {
+            kind: Kind::MultipleCredentialsTypes,
+        }
+    }
+}
+
 #[derive(Debug)]
 enum Kind {
-    Unknown,
+    ConversionError(ToStrError),
+    CredentialsNotProvided(String),
+    MultipleCredentialsTypes,
 }
