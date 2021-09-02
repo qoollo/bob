40d059d3b6d279eb281d2a2a12e3ab21d1cabadb
Pavel Yakushin <JetPilot412@gmail.com>
2021-09-02 15:35:26 +0300

    add nodes creds check, add nodes creds validation

--- bob-access/src/authenticator/basic.rs
+++ bob-access/src/authenticator/basic.rs
@@ -16,26 +16,44 @@ impl<Storage: UsersStorage> Basic<Storage> {
         }
     }
 
-    pub fn set_nodes_credentials(&mut self, nodes: Vec<Credentials>) {
-        self.nodes = nodes;
+    pub fn set_nodes_credentials(&mut self, nodes: Vec<Credentials>) -> Result<(), Error> {
+        if nodes
+            .iter()
+            .all(|cred| cred.ip().is_some() && cred.username().is_some())
+        {
+            self.nodes = nodes;
+            Ok(())
+        } else {
+            let message = "nodes credentials missing ip or username";
+            Err(Error::credentials_not_provided(message))
+        }
+    }
+
+    fn is_node(&self, other: &Credentials) -> bool {
+        self.nodes
+            .iter()
+            .any(|cred| cred.ip() == other.ip() && cred.username() == other.username())
     }
 }
 
 impl<Storage: UsersStorage> Authenticator for Basic<Storage> {
     fn check_credentials(&self, credentials: Credentials) -> Result<(), Error> {
-        if let Some(username) = credentials.username() {
-            let user = self.users_storage.get_user(username)?;
-            if let Some(password) = credentials.password() {
-                if user.password() == password {
-                    Ok(())
-                } else {
-                    Err(Error::unauthorized_request())
-                }
-            } else {
-                Err(Error::credentials_not_provided("missing password"))
-            }
+        if self.is_node(&credentials) {
+            debug!("received request from node: {:?}", credentials.username());
+            return Ok(());
+        }
+        let username = credentials
+            .username()
+            .ok_or_else(|| Error::credentials_not_provided("missing username"))?;
+        let password = credentials
+            .password()
+            .ok_or_else(|| Error::credentials_not_provided("missing password"))?;
+
+        let user = self.users_storage.get_user(username)?;
+        if user.password() == password {
+            Ok(())
         } else {
-            Err(Error::credentials_not_provided("missing username"))
+            Err(Error::unauthorized_request())
         }
     }
 }
