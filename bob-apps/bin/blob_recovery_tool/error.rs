85b2b5cd99dfec7be982294dd89c40094dbac564
Vladimir Stepanenko <vovac12@gmail.com>
2021-10-19 21:13:48 +0300

    Blob recovery tool with verification (#262)
    * First version
    
    * Add logging and checksum validation
    
    * Revert some changes
    
    * Remove duplicate validation code
    
    * Split blob reader and writer
    
    * Add validation of every N written records
    
    * Add verification to brt
    
    * Add index validation
    
    * Fix logging
    Add index delete operation
    Add blob recovery operation
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    * Refactoring
    
    * Fix PR issue
    
    * Fix review issues
    
    * Fix review issues
    
    * Fix warnings
    
    * Trying to skip wrong record data
    
    * Fix review issues

---
+++ bob-apps/bin/blob_recovery_tool/error.rs
@@ -0,0 +1,44 @@
+use super::prelude::*;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub(crate) enum Error {
+    #[error("record header validation error: {0}")]
+    RecordHeaderValidation(String),
+    #[error("index header validation error: {0}")]
+    IndexHeaderValidation(String),
+    #[error("index validation error: {0}")]
+    IndexValidation(String),
+    #[error("blob header validation error: {0}")]
+    BlobHeaderValidation(String),
+    #[error("record validation error: {0}")]
+    RecordValidation(String),
+    #[error("skip data error: {0}")]
+    SkipRecordData(String),
+}
+
+impl Error {
+    pub(crate) fn record_header_validation_error(message: impl Into<String>) -> Self {
+        Self::RecordHeaderValidation(message.into())
+    }
+
+    pub(crate) fn index_header_validation_error(message: impl Into<String>) -> Self {
+        Self::IndexHeaderValidation(message.into())
+    }
+
+    pub(crate) fn index_validation_error(message: impl Into<String>) -> Self {
+        Self::IndexValidation(message.into())
+    }
+
+    pub(crate) fn blob_header_validation_error(message: impl Into<String>) -> Self {
+        Self::BlobHeaderValidation(message.into())
+    }
+
+    pub(crate) fn record_validation_error(message: impl Into<String>) -> Self {
+        Self::RecordValidation(message.into())
+    }
+
+    pub(crate) fn skip_record_data_error(message: impl Into<String>) -> Self {
+        Self::SkipRecordData(message.into())
+    }
+}
