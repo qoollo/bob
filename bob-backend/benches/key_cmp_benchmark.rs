d3dca48995f1b90cae324536a6ce20a0f288f0bd
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2022-06-07 22:37:14 +0300

    fix bench error

--- bob-backend/benches/key_cmp_benchmark.rs
+++ bob-backend/benches/key_cmp_benchmark.rs
@@ -4,7 +4,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("cmp size 8", |b| {
         b.iter(|| {
-            le_cmp_keys_opt(
+            le_cmp_keys(
                 black_box(&[3, 1, 1, 1, 1, 1, 1, 1]),
                 black_box(&[2, 1, 1, 1, 1, 1, 1, 1]),
             )
