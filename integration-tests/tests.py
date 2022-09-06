5a1705f6743d7e27d8b01f325eb84f593b5cdae8
Max <maxim.panov@yandex.ru>
2022-09-06 19:44:15 +0300

    added handlig of built from source binaries

--- integration-tests/tests.py
+++ integration-tests/tests.py
@@ -21,9 +21,11 @@ for (key) in final_args:
         args_str += f'{key} {final_args.get(key)} '
 
 
+binaries_dir = f'./target/{os.getenv("TARGET")}/release'
+
 #write/read tests
 try:
-    p = subprocess.check_output(shlex.split(f'../bobp -b put {args_str.rstrip()}'))
+    p = subprocess.check_output(shlex.split(f'{binaries_dir}/bobp -b put {args_str.rstrip()}'))
     print(str(p))
 except subprocess.CalledProcessError as e:
     print(e)
@@ -31,7 +33,7 @@ except subprocess.CalledProcessError as e:
 
 
 try:
-    p = subprocess.check_output(shlex.split(f'../bobp -b get {args_str.rstrip()}'))
+    p = subprocess.check_output(shlex.split(f'{binaries_dir}/bobp -b get {args_str.rstrip()}'))
     print(str(p))
 except subprocess.CalledProcessError as e:
     print(e)
