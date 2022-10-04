a249f04cc06185134e9e368c99afa2980bd93bb0
Max <maxim.panov@yandex.ru>
2022-10-04 21:45:17 +0300

    added shell command execution

--- integration-tests/tests.py
+++ integration-tests/tests.py
@@ -12,7 +12,7 @@ def run_tests(behaviour, args):
                 sys.exit(f'{behaviour} test failed, see output')
         elif behaviour == 'exist':
             print(f'Running bobp -b {str(behaviour)} {args.rstrip()}')
-            p = subprocess.check_output(shlex.split(f'./bobp -b {behaviour} {args.rstrip()}'))
+            p = subprocess.check_output(shlex.split(f'./bobp -b {behaviour} {args.rstrip()}'), shell=True)
             print(str(p))
             found_exist = re.search(r'\b[0-9]{1,}\sof\s[0-9]{1,}\b', str(p))
             if not found_exist:
