34a5ceb538fbd05f209ff1770d2f526aba7e25a6
Max <mijijka@gmail.com>
2022-10-13 11:42:57 +0300

    added correct first index processing

--- integration-tests/tests.py
+++ integration-tests/tests.py
@@ -40,14 +40,13 @@ def run_tests(behaviour, args):
         sys.exit(str(e))
 
 def get_run_args(mode, args):
-    return {'-c':args.count, '-l':args.payload, '-h':f'{args.node}', '-s':args.start, '-e':args.end, '-t':args.threads, '--mode':args.mode, '-k':args.keysize, '-p':test_run_config.get(mode)}
+    return {'-c':args.count, '-l':args.payload, '-h':f'{args.node}', '-f':args.first, '-t':args.threads, '--mode':args.mode, '-k':args.keysize, '-p':test_run_config.get(mode)}
 
 parser = argparse.ArgumentParser(description='This script launches bob tests with given configuration.')
 parser.add_argument('-c', dest='count', type=int, help='amount of entries to process', required=True)
 parser.add_argument('-l', dest='payload', type=int, help='payload in bytes', required=True)
 parser.add_argument('-n', dest='node', type=str, help='target node address', required=True)
-parser.add_argument('-s', dest='start', type=int, help='start index', default=0)
-parser.add_argument('-e', dest='end', type=int, help='end index')
+parser.add_argument('-f', dest='first', type=int, help='first index', default=0)
 parser.add_argument('-t', dest='threads', type=int, help='amount of working threads', default=1)
 parser.add_argument('--mode', dest='mode', type=str, help='random or normal', choices=['random', 'normal'], default='normal')
 parser.add_argument('-k', dest='keysize', type=int, help='size of binary key (8 or 16)', choices=[8, 16], default=8)
@@ -58,8 +57,6 @@ parsed_args = parser.parse_args()
 for item in run_options:
     args_str = str()
     run_args = get_run_args(item, parsed_args)
-    if item == 'exist':
-        run_args.pop('-s')
     for key in run_args:
         if run_args.get(key) != None:
             args_str += f'{key} {run_args.get(key)} '
