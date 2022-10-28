6bb2d0347f74c643ddbc475e6d58b2c54f22ae6d
Max <maxim.panov@yandex.ru>
2022-10-28 20:29:05 +0300

    moved timer logic for bob nodes to separate file

--- integration-tests/deploy.py
+++ integration-tests/deploy.py
@@ -1,39 +1,11 @@
 #!/usr/bin/python3
-import os, argparse, sys, subprocess, shlex, docker, requests, json
+import os, argparse, sys, subprocess, shlex, docker
 from python_on_whales import docker as d_cli
 from docker import errors as d_err
 from docker import types as d_types
 from retry import *
+from bob_backend_timer import ensure_backend_up
 
-
-def request_metrics(port):
-    try:
-        return requests.get(f"http://0.0.0.0:{port}/metrics")
-    except requests.RequestException:
-        sys.exit('Failed to get backend status.')
-    
-         
-@retry((UserWarning), tries=5, delay=1, backoff=2, max_delay=5)
-def ensure_backend_up():
-    try:
-        for item in range(int(os.environ['BOB_NODES_AMOUNT'])):
-            response = request_metrics(f"800{item}")
-            if response.status_code != 200:
-                print(f'Failed to get response from bob instance {item}, retrying...')
-                raise UserWarning(f'Timeout on getting response from node {item}.')
-            else:
-                metrics = json.loads(response.content.decode('ascii'))
-                if metrics['metrics']['backend.backend_state']['value'] != 1:
-                    print(f'Backend is not up on node {item}, waiting...')
-                    raise UserWarning(f'Backend is down on node {item}.')
-                else:
-                    print(f'Node {item} is ready!')
-        print('All nodes are ready!')
-    except KeyError:
-        sys.exit('Nodes amount is not set.')
-    except ValueError:
-        sys.exit('Amount of nodes has unexpected value.')
-    
 
 #collect arguments
 parser = argparse.ArgumentParser(description='Deploys docker compose nodes.')
