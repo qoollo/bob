6f8f30dc987a184cac82d849d9f10977e159633b
Max <maxim.panov@yandex.ru>
2022-10-30 13:39:41 +0300

    fixed print node status

--- integration-tests/tests-aliens.py
+++ integration-tests/tests-aliens.py
@@ -53,7 +53,7 @@ try:
         bobp_args = args_to_str(dict_args)
         #run put
         try:
-            print(f'Bob logs on write node:\n' + re.findall(r"Node status: (local_node_\d{1,} )*local_node_\d{1,}", str(d_cli.container.logs(container_dict[dict_args['-p']]))))
+            print(f'Bob logs on write node:\n', re.findall(r"Node status: (local_node_\d{1,} )*local_node_\d{1,}", str(d_cli.container.logs(container_dict[dict_args['-p']]))))
         except DockerException as e:
             sys.exit(e.stderr)
         print(f'Running bobp -b put {bobp_args.rstrip()}')
