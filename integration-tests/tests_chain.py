ebe5f0b8fc3af5b6e316934bf825fd5c375a2896
Max <maxim.panov@yandex.ru>
2022-11-06 18:19:33 +0300

    fixed args conversion

--- integration-tests/tests_chain.py
+++ integration-tests/tests_chain.py
@@ -22,7 +22,7 @@ bobt_args = args_to_str(make_run_args(parsed_args))
 
 try:
     print(f'Running bobt {bobt_args.rstrip()}')
-    p = subprocess.check_output(shlex.split(f'./bobt {bobt_args.rsplit()}')).decode('ascii')
+    p = subprocess.check_output(shlex.split(f'./bobt {bobt_args.rstrip()}')).decode('ascii')
     print(str(p))
     #TODO: add bobt output handling 
 except subprocess.CalledProcessError as e:
