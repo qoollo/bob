7d2afd6360a0a4a44558542ae933053730cc7db2
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-12-23 12:57:15 +0300

    Move allocator def into a separate file

--- bob-apps/bin/bobd.rs
+++ bob-apps/bin/bobd.rs
@@ -1,19 +1,4 @@
-#[cfg(all(
-    any(feature = "mimalloc", feature = "mimalloc-secure"),
-    target_arch = "x86_64",
-    target_env = "musl",
-    target_pointer_width = "64"
-))]
-use mimalloc::MiMalloc;
-
-#[cfg(all(
-    any(feature = "mimalloc", feature = "mimalloc-secure"),
-    target_arch = "x86_64",
-    target_env = "musl",
-    target_pointer_width = "64"
-))]
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
+include!("alloc/mimalloc.rs");
 
 use bob::{
     build_info::BuildInfo, init_counters, BobApiServer, BobServer, ClusterConfig, NodeConfig, Factory, Grinder,
