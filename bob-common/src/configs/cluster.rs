e182e495e4afd0631da6bd913f47fa5959867213
ikopylov <ikopylov@users.noreply.github.com>
2023-02-23 01:23:14 +0100

    [774] Build fix

--- bob-common/src/configs/cluster.rs
+++ bob-common/src/configs/cluster.rs
@@ -442,7 +442,7 @@ impl Validatable for Cluster {
         let vdisk_ids = self.vdisks.iter().map(|vdisk| vdisk.id).collect::<Vec<_>>();
         vdisk_ids.sort();
         for index in 0..vdisk_ids.len() {
-            if vdisk_ids[index].into() < index as u32 {
+            if vdisk_ids[index] < index as u32 {
                 error!("config contains duplicates vdisks ids: {}", vdisk_ids[index]);
                 return Err(format!("config contains duplicates vdisks ids: {}", vdisk_ids[index]));
             } else if vdisk_ids[index] > index as u32 {
