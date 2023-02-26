392a6672540fb10d86edf5f199625ecaf735f278
ikopylov <ikopylov@users.noreply.github.com>
2023-02-26 23:07:22 +0100

    [774] Fix build

--- bob/src/api/mod.rs
+++ bob/src/api/mod.rs
@@ -19,7 +19,8 @@ use bob_access::{Authenticator, CredentialsHolder};
 use bob_backend::pearl::{Group as PearlGroup, Holder, NoopHooks};
 use bob_common::{
     configs::node::TLSConfig,
-    data::{BobData, BobKey, BobMeta, VDisk as DataVDisk, BOB_KEY_SIZE},
+    data::{BobData, BobKey, BobMeta, BOB_KEY_SIZE},
+    core_types::{VDisk as DataVDisk},
     operation_options::{BobPutOptions, BobGetOptions, BobDeleteOptions},
     error::Error as BobError,
     node::Disk as NodeDisk,
