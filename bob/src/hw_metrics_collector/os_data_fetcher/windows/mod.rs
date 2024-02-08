8c1ba6a462285a9e5dc7b08fad8dab207def6630
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2024-02-08 12:58:50 +0300

    730 add disks search

--- bob/src/hw_metrics_collector/os_data_fetcher/windows/mod.rs
+++ bob/src/hw_metrics_collector/os_data_fetcher/windows/mod.rs
@@ -1,6 +1,7 @@
-use std::collections::{HashSet, HashMap};
+use std::{collections::{HashMap, HashSet}, path::Path};
 use super::{DiskSpaceMetrics, OsDiskMetrics, OsDataMetrics};
 use bob_common::core_types::{PhysicalDiskId, DiskPath};
+use sysinfo::{DiskExt, System, SystemExt};
 
 #[derive(Clone)]
 pub(crate) struct OsDataFetcher {
@@ -8,8 +9,10 @@ pub(crate) struct OsDataFetcher {
 }
 
 impl OsDataFetcher {
-    pub async fn new(_bob_disks: &[DiskPath]) -> Self {
-        Self { disks: HashSet::default() }
+    pub async fn new(bob_disks: &[DiskPath]) -> Self {
+        let disks = find_physical_disks(bob_disks).await;
+        println!("{:?}", disks);
+        Self { disks }
     }
 
     pub async fn collect(&mut self) -> OsDataMetrics {
@@ -20,3 +23,25 @@ impl OsDataFetcher {
         DiskSpaceMetrics::default()
     }
 }
+
+async fn find_physical_disks(bob_disks: &[DiskPath]) -> HashSet<PhysicalDiskId> {
+    System::new_all()
+        .disks()
+        .iter()
+        .filter_map(|d| {
+            let path = std::fs::canonicalize(d.mount_point())
+                .expect("canonicalize mount path");
+            if bob_disks
+                .iter()
+                .any(move |dp| {
+                    let dp = std::fs::canonicalize(dp.path())
+                        .expect("canonicalize bob path");
+                    dp.starts_with(&path)
+                }) {
+                Some(d.mount_point().to_str().unwrap().into())
+            } else {
+                None
+            }
+        })
+        .collect()
+} 
