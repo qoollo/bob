6f8822c893ef09505639c67d99fb2be0472d3765
Ivan Druzhitskiy <idruzhitskiy@gmail.com>
2024-02-27 09:47:33 +0300

    730 switch to linux/generic os fetchers instead of target-based

--- bob/src/hw_metrics_collector/os_data_fetcher/mod.rs
+++ bob/src/hw_metrics_collector/os_data_fetcher/mod.rs
@@ -14,40 +14,12 @@ pub(super) struct OsDataMetrics {
     pub(super) disk_metrics: HashMap<PhysicalDiskId, OsDiskMetrics>,
 }
 
-#[cfg(target_family = "unix")]
-mod unix;
-#[cfg(target_family = "unix")]
-pub(crate) use unix::OsDataFetcher;
+#[cfg(target_os = "linux")]
+mod linux;
+#[cfg(target_os = "linux")]
+pub(crate) use linux::OsDataFetcher;
 
-#[cfg(target_family = "windows")]
-mod windows;
-#[cfg(target_family = "windows")]
-pub(crate) use windows::OsDataFetcher;
-
-#[cfg(not(any(target_family = "unix", target_family = "windows")))]
-mod default {
-    use std::collections::{HashSet, HashMap};
-    use super::{DiskSpaceMetrics, OsDiskMetrics, OsDataMetrics};
-    use bob_common::core_types::{PhysicalDiskId, DiskPath};
-
-    #[derive(Clone)]
-    pub(crate) struct OsDataFetcher {
-        disks: HashSet<PhysicalDiskId>
-    }
-
-    impl OsDataFetcher {
-        pub async fn new(_bob_disks: &[DiskPath]) -> Self {
-            Self { disks: HashSet::default() }
-        }
-
-        pub async fn collect(&mut self) -> OsDataMetrics {
-            OsDataMetrics::default()
-        }
-
-        pub async fn collect_space_metrics(&self) -> DiskSpaceMetrics {
-            DiskSpaceMetrics::default()
-        }
-    }
-}
-#[cfg(not(any(target_family = "unix", target_family = "windows")))]
-pub(crate) use default::OsDataFetcher;
+#[cfg(not(target_os = "linux"))]
+mod generic;
+#[cfg(not(target_os = "linux"))]
+pub(crate) use generic::OsDataFetcher;
